46aa11c974d4a836b36d88d345b945820abbc5d3
HerzA2 <herzbergak@hotmail.com>
2016-12-01 14:21:04 -0600

    Update sr_router.c

--- cs640_project2/router/sr_router.c
+++ cs640_project2/router/sr_router.c
@@ -333,11 +333,20 @@ void sr_handlepacket(struct sr_instance* sr,
 
   //change header fields
 
+  /*
+Sanity-check the packet (meets minimum length and has correct checksum).
+Decrement the TTL by 1, and recompute the packet checksum over the modified header.
+Find out which entry in the routing table has the longest prefix match with the destination IP address.
+  */
+	//Dest Mac Addres / Source Mac address / Ether Type
+	// 6 Bytes, 6 Bytes, 2 Bytes
+	//Ether Type = 0x0800 for IPv4 (We only deal with IPv4 right?
+	//Ether Type = 0x0806 for ARP
   //Determine if ARP
   //if so call sr_handlepacket_arp(struct sr_instance *sr, uint8_t *pkt, unsigned int len, struct sr_if *src_iface)
   //if not ARP, 
   //check if address matches router (if so, port unreachable type 3, code 3 error)
-  //if not, check arp cache for address => call the sr_arpcache_lookup
+  //if not, check arp cache for address => call the sr_arpcache_lookup, (sr_waitforarp if missing?)
   //if not found, check requests (automatically adds packet to request list)
 
 
